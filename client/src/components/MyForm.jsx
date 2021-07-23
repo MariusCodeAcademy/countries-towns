116f32af99ed2caf8f0494dc660f8278d3d1d095
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-07-23 09:24:34 +0300

    name input 2 way binding

--- client/src/components/MyForm.jsx
+++ client/src/components/MyForm.jsx
@@ -2,25 +2,53 @@ import React, { Component } from 'react';
 
 class MyForm extends Component {
   state = {
-    name: '',
+    name: 'USA',
     continent: '',
     population: null,
     placeType: '',
   };
+
+  handleSubmitLocal = (e) => {
+    e.preventDefault();
+    console.log('stop right there');
+  };
+
+  handleInput = (e) => {
+    this.setState({ name: e.target.value });
+  };
+
   render() {
+    const { state: s } = this;
     return (
       <div className="w-50">
         <h2>Create new place</h2>
-        <form>
+        <form onSubmit={this.handleSubmitLocal} autoComplete="off">
           <div className="form-group">
-            <input type="text" className="form-control" name="name" placeholder="Title" />
+            <input
+              value={s.name}
+              onChange={this.handleInput}
+              type="text"
+              className="form-control"
+              name="name"
+              placeholder="Name of Place"
+            />
           </div>
           <div className="form-group">
-            <input type="text" className="form-control" name="continent" placeholder="Title" />
+            <input type="text" className="form-control" name="continent" placeholder="Continent" />
           </div>
           <div className="form-group">
-            <input type="number" className="form-control" name="population" placeholder="Title" />
+            <input
+              type="number"
+              className="form-control"
+              name="population"
+              placeholder="Population"
+            />
           </div>
+          <select name="placeType" className="custom-select">
+            <option value="town">Town</option>
+            <option value="country">Country</option>
+          </select>
+          <button className="btn btn-primary my-4">Create</button>
         </form>
       </div>
     );
