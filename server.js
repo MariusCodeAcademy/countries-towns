e2d2a1e3194a74486c2370289c76ff562ef205e8
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-07-23 08:40:46 +0300

    in dotEnv, and connect to db

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 // MiddleWare
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
 
-app.listen(3000);
+// prisijungimas prie duomenu bazes
+mongoose
+  .connect(process.env.MONGO_CONN_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    console.log('Conneced to Mongo ooooooooose');
+  })
+  .catch((err) => console.error(err.message));
+
+app.listen(PORT, console.log(`Server is running on port ${PORT}`));
