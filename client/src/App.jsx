18f8741cc6506ab9c91e378ad3be98a5f13fcb51
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-07-23 14:24:14 +0300

    refactor axios to separate file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import MyForm from './components/MyForm';
-import axios from 'axios';
 import PlaceList from './components/PlaceList';
+import { getPlaces, updatePlaceSend, createPlaceSend, deletePlaceSend } from './utils/request';
 
 class App extends Component {
   constructor(props) {
@@ -17,62 +17,31 @@ class App extends Component {
   }
 
   createNewPlace = async (dataToCreateNewPlace) => {
-    console.log('createNewPlace in app.jsx Fired');
-    console.log('dataToCreateNewPlace', dataToCreateNewPlace);
-    try {
-      const createResult = await axios.post(
-        'http://localhost:4000/api/place/new',
-        dataToCreateNewPlace
-      );
+    const success = await createPlaceSend(dataToCreateNewPlace);
+    if (success) {
       // atnaujinti sarasa places po ivedimo
       this.getAllPlaces();
-      console.log('createResult', createResult.data);
-      return createResult.data ? true : false;
-    } catch (error) {
-      console.error(error);
+      return true;
     }
   };
 
   getAllPlaces = async () => {
     // gaunam visas vietas is db
-    try {
-      const allPlacesFromDb = await axios.get('http://localhost:4000/api/place');
-      // jei gavom duomenis
-      console.log('inside Try');
-      if (Array.isArray(allPlacesFromDb.data) && allPlacesFromDb.data.length) {
-        // nustatom state i tai ka gavom
-        this.setState({ places: allPlacesFromDb.data });
-      }
-    } catch (error) {
-      console.error(error);
-    }
+    const placesArr = await getPlaces();
+    this.setState({ places: placesArr });
   };
 
   deletePlace = async (id) => {
-    console.log('you want to delete', id);
-    try {
-      const deleteResult = await axios.delete('http://localhost:4000/api/place/delete/' + id);
-      // console.log('deleteResult', deleteResult.data);
-      // atnaujinti sarasa kad neliktu ka istrynem
-      if (deleteResult.data) {
-        this.getAllPlaces();
-      }
-    } catch (error) {
-      console.error(error);
+    const success = await deletePlaceSend(id);
+    if (success) {
+      this.getAllPlaces();
     }
   };
 
   updatePlace = async (id, updatedDetails) => {
     console.log('about to update place', id, updatedDetails);
-    try {
-      const updateResult = await axios.put(
-        'http://localhost:4000/api/place/update/' + id,
-        updatedDetails
-      );
-      if (updateResult.data) this.getAllPlaces();
-    } catch (error) {
-      console.error(error);
-    }
+    const success = await updatePlaceSend(id, updatedDetails);
+    success && this.getAllPlaces();
   };
 
   render() {
