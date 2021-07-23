02c0e3817dd5418c86c384d28748d766ee4a1278
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-07-23 08:33:24 +0300

    server runnig, middleware

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-// start of back end
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const app = express();
+
+// MiddleWare
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors());
+
+app.listen(3000);
