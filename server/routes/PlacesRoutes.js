31f2c582297084600a746675f4473e5acec6790b
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-07-23 10:24:55 +0300

    create new place works . server

--- server/routes/PlacesRoutes.js
+++ server/routes/PlacesRoutes.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const PlaceModel = require('../models/Place');
 const router = express.Router();
 
 // create new place
-router.post('/api/place/new', (req, res) => {
-  res.json('you are about to create a place');
+router.post('/api/place/new', async (req, res) => {
+  const gotNewPlaceData = req.body;
+  console.log(' gotNewPlaceData', gotNewPlaceData);
+  //   res.json('you are about to create a place');
+  // naujos vietos sukurimas naudojant modeli
+  const newPlace = new PlaceModel(gotNewPlaceData);
+  try {
+    const creatingNewPlaceResult = await newPlace.save();
+    res.json(creatingNewPlaceResult);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
